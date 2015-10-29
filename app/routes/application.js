257776d32dc300f0c0dde8d2904274172e2067d5
Gabriel Grant <g@briel.ca>
2015-10-29 08:42:47 -0400

    add manual tracking of ember login state for testing

--- app/routes/application.js
+++ app/routes/application.js
@@ -7,14 +7,31 @@ export default Ember.Route.extend({
       let provider="password";
       let email = controller.get("email");
       let password = controller.get("password");
+      if (Ember.testing){
+        var waiter = (() => false);
+        Ember.Test.registerWaiter(waiter);
+      }
       this.get("session").open("firebase", { provider, email, password }).then(() => {
+        if (Ember.testing){
+          Ember.Test.unregisterWaiter(waiter);
+        }
         this.transitionTo("projects");
       }, (error) => {
+        if (Ember.testing){
+          Ember.Test.unregisterWaiter(waiter);
+        }
         controller.set("error", error);
       });
     },
     signOut() {
+      if (Ember.testing){
+        var waiter = (() => false);
+        Ember.Test.registerWaiter(waiter);
+      }
       this.get("session").close().then(() => {
+        if (Ember.testing){
+          Ember.Test.unregisterWaiter(waiter);
+        }
         this.transitionTo("signin");
       });
     }
